5ccaa814db35ebb3ca77ef16665ab0d46d4d52e7
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-10-24 16:30:46 +0300

    submit first assignment

---
+++ Day1/First Java Program/Greeting.java
@@ -0,0 +1,7 @@
+public class Greeting{
+    public static void main(String[] args) {
+        System.out.println("My Name is: Hasan Sadaqa");
+        System.out.println("I'm 24 Years old");
+        System.out.println("my hoetown is Jenin, Palestine");
+    }
+}
