2e572307e52371daed5c93d35cefbdb63ad51b99
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-10-30 18:05:48 +0300

    adding main method

---
+++ Day3/Orders & Items/TestOrders.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class TestOrders{
+    public static void main(String[] args){
+        Item item = new Item();
+        Order order = new Order();
+        
+    }
+}
