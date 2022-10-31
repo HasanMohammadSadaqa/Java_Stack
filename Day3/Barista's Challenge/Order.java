97c6cb96ea1dac298f43701fcbdfa5abe2fb7724
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-10-31 07:15:42 +0300

    creating a constructer,setter and getter in Order class

--- Day3/Barista's Challenge/Order.java
+++ Day3/Barista's Challenge/Order.java
@@ -1,8 +1,44 @@
 import java.util.ArrayList;
 
 public class Order{
+    //member variables
     private String name;
-    private double total;
     private boolean ready;
-    private ArrayList<Object> items = new ArrayList<Object>();
+    private ArrayList<Item> items;
+
+    //constructor 
+    public Order(){
+        this.name = "Guest";
+        this.items = new ArrayList<Item>();
+    }
+
+    //Overloaded Constructor: 
+    public Order(String name){
+        this.name = name;
+    }
+
+    //Mutators(Setters)
+    public void setName(String n){
+        this.name = n;
+    }
+    public void setReady(Boolean isReady){
+        this.ready = isReady;
+    }
+    public void setItems(ArrayList<Item> items){
+        this.items = items;
+    }
+
+    //Accessors(Getters)
+    public String getName(){
+        return name;
+    }
+    public boolean isReady(){
+        return ready;
+    }
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    //Methods
+
 }
