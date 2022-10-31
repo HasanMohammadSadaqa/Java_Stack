87dc7dbe877a0ac431f8893dd4105b3515e7416c
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-10-31 14:29:16 +0300

    no changes unless the folder name which change all thing, so it;s the final submition

---
+++ Day3/Barista_Challenge/Item.java
@@ -0,0 +1,30 @@
+public class Item {
+
+    // member variables
+    private String name;
+    private double price;
+
+    // setter
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public void setPrice(double P) {
+        this.price = P;
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // constructor
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
