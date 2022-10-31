1981e429de3fc1fc4fe991f6cd306181d2e893d4
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-10-31 06:45:11 +0300

    creating all classes i need

---
+++ Day3/Barista's Challenge/Order.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Order{
+    private String name;
+    private double total;
+    private boolean ready;
+    private ArrayList<Object> items = new ArrayList<Object>();
+}
