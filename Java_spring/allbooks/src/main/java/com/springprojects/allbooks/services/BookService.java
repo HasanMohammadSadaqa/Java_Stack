577135cf89e42ac1a74ee7dd3e27aea5dec94dda
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-11-09 14:56:13 +0300

    bookApiassignment

---
+++ Java_spring/allbooks/src/main/java/com/springprojects/allbooks/services/BookService.java
@@ -0,0 +1,47 @@
+package com.springprojects.allbooks.services;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.springprojects.allbooks.models.Book;
+import com.springprojects.allbooks.repositories.BookRepository;
+
+@Service
+public class BookService {
+	 private final BookRepository bookRepository;
+	    
+	    public BookService(BookRepository bookRepository) {
+	        this.bookRepository = bookRepository;
+	    }
+	    // returns all the books
+	    public List<Book> allBooks() {
+	        return bookRepository.findAll();
+	    }
+	    // creates a book
+	    public Book createBook(Book b) {
+	        return bookRepository.save(b);
+	    }
+	    // retrieves a book
+	    public Book findBook(Long id) {
+	        Optional<Book> optionalBook = bookRepository.findById(id);
+	        if(optionalBook.isPresent()) {
+	            return optionalBook.get();
+	        } else {
+	            return null;
+	        }
+	    }
+	    
+	    public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+	        Book book = bookRepository.findById(id).get();
+	        book.setTitle(title);
+	        book.setLanguage(lang);
+	        book.setDescription(desc);
+	        book.setNumberOfPages(numOfPages);
+	        return bookRepository.save(book);
+	    }
+	    
+	    public void deleteBook(Long id) {
+	        bookRepository.deleteById(id);
+	       }
+}
